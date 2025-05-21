fef00057765b4c5164e844ad2901d72b14590998
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2025-05-21 17:17:19 +0530

    learning GO part 2

--- userInput/main.go
+++ userInput/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	welcome := "welcome to user input"
 	fmt.Println(welcome)
 
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our pizza :")
+
+	//comma ok or err ok syntax
+	input, _ := reader.ReadString('\n')
+	fmt.Println("thanks for waiting : ", input)
 }

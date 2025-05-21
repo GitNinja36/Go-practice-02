bcf93133bd4fb10efde951845b811f93d556faef
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2025-05-21 16:49:42 +0530

    starting learning GO part 2

---
+++ userInput/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	welcome := "welcome to user input"
+	fmt.Println(welcome)
+
+}
--- variable/variables.go
+++ variable/variables.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var username string = "rohit"
+	fmt.Printf("variable is of type : %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Printf("variable is of type : %T \n", isLoggedIn)
+
+	numberOfuser := 30000
+	fmt.Println(numberOfuser)
 }
